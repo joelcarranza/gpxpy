8a0011d7aa006b2cba7118ba86a0c61831d699ce
Joel Carranza <joel.carranza@gmail.com>
2011-02-12 09:06:09 -0800

    more pythonic API. added utility methods

--- GPX.py
+++ GPX.py
@@ -10,8 +10,9 @@ import xml.etree.ElementTree as ElementTree
 from xml.etree.ElementTree import Element
 import sys
 import os
+# this is going to fail on 2.5???
 import dateutil.parser
-
+import math
 
 class GPXParser:
   NS = '{http://www.topografix.com/GPX/1/1}'
@@ -51,8 +52,8 @@ class GPXParser:
     for seg in el.findall("%strkseg" % self.NS):
       p = Path()
       for wpel in seg.findall("%strkpt" % self.NS):
-        p.points.append(self.parseWaypoint(wpel))
-      trk.segments.append(p)
+        p.append(self.parseWaypoint(wpel))
+      trk.append(p)
     self.mapEl(trk,el,{
       "name":"s",
       "cmt":"s",
@@ -119,7 +120,7 @@ class GPX:
       root.append(route.toxml("rte","rtept"))    
     for track in self.tracks:
       trk = Element("trk")
-      for seg in track.segments:
+      for seg in track:
         trk.append(seg.toxml("trgseg","trkpt"))
       root.append(trk)
     return root
@@ -130,22 +131,62 @@ class GPX:
   
   def __str__(self):
     return "GPX{waypoints=%d,tracks=%d,routes=%d}" % (len(self.waypoints),len(self.tracks),len(self.routes))
+  
 class Path:
-  points = None
+  """
+  Ordered list of points
+  """
+  _wpt = None
   
   def __init__(self):
-    self.points = []
+    self._wpt = []
+  
+  def points(self):
+    "Return a list of waypoints in the path"
+    return self._wpt
+    
+  def append(self,wpt):
+    "Add a new waypoint to the end of the path"
+    self._wpt.append(wpt)
+    
+  def extend(self,pts):
+    "Add new waypoints to the end of the path"
+    self._wpt.extend(pts)
+    
+  def filter(self,pred):
+    "Remove point from this path based on predicate"
+    self._wpt = filter(pred,self._wpt)
+    
+  def simplify(self,pred):
+    """Remove points form path based on predicate. Predicate
+    is passed two points, the current point and the last point
+    that passed the test. Only points that pass the test are returned""" 
+    np = self._wpt[0:1]
+    for p in self._wpt[1:]:
+      if pred(p,np[-1]):
+        np.append(p)
+    self._wpt = np
+    
+  def __iter__(self):
+    return self._wpt.__iter__()
+  
+  def __len__(self):
+    return len(self._wpt)
+  
+  def __getitem__(self,k):
+    return self._wpt[k]
   
   def bounds(self):
     "return a bounding box which contains all waypoints on this path"
-    if len(self.points) == 0:
+    if len(self._wpt) == 0:
       return None
-    lat = [p.lat for p in self.points]
-    lon = [p.lon for p in self.points]
+    lat = [p.lat for p in self._wpt]
+    lon = [p.lon for p in self._wpt]
     return (min(lat),min(lon),max(lat),max(lon))
   
   def timespan(self):
-    t = [p.time for p in self.points if p.time is not None]
+    "return min max bounds of path"
+    t = [p.time for p in self._wpt if p.time is not None]
     if len(t) > 0:
       return (min(t),max(t))
     else:
@@ -153,14 +194,17 @@ class Path:
   
   def toxml(self,name,ptname):
     e = Element(name)
-    for p in self.points:
+    for p in self._wpt:
       e.append(p.toxml(ptname))
     return e
     
   def __str__(self):
-    return "Path{points=%d}" % len(self.points)
+    return "Path{points=%d}" % len(self._wpt)
 
 class Route(Path):
+  """
+  n ordered list of waypoints representing a series of turn points leading to a destination.
+  """
   name = None
   cmt = None
   src = None
@@ -168,27 +212,62 @@ class Route(Path):
   link = None
   
 class Track:
+  """
+  Represents an ordered list of track segments (paths) which
+  taken together represent a (potentially complex) path
+  """
   name = None
   cmt = None
   src = None
   desc = None
   link = None
-  segments = None
+  _s = None
   
   def __init__(self):
-    self.segments = []
+    self._s = []
+  
+  def points(self):
+    "Returns an interator over all waypoints in track"
+    for s in self._s:
+      for p in s:
+        yield p
   
   def join(self):
     "Join all segments together into a single segment"
-    for s in self.segments[1:]:
-      self.segments[0].points.extend(s.points)
-    self.segments[1:] = []
+    for s in self._s[1:]:
+      self._s[0].extend(s.points)
+    self._s[1:] = []
+  
+  def segments(self):
+    return self._s
     
+  def append(self,trk):
+    "Add a new waypoint to the end of the path"
+    self._s.append(trk)
+
+  def extend(self,trk):
+    "Add new waypoints to the end of the path"
+    self._s.extend(trk)
+  
+  def __iter__(self):
+    return self._s.__iter__()
+
+  def __len__(self):
+    return len(self._s)
+
+  def __getitem__(self,k):
+    return self._s[k]
+  
   def __str__(self):
-    return "Track{segments=%d}" % len(self.segments)
+    return "Track{segments=%d}" % len(self._s)
   
 
 class Waypoint:
+  """
+  Represents a waypoint, point of interest, or named feature on a map.
+  See: http://www.topografix.com/GPX/1/1/#type_wptType
+  """
+  
   lat = None
   lon = None
   ele = None
@@ -219,6 +298,20 @@ class Waypoint:
     
   def __str__(self):
     return "Waypoint{%d,%d}" % (self.lat,self.lon)
+  
+  def dist(self,p):
+    "Distance between two waypoints using haversine"
+    # TODO: take into account of elevation!
+    R = 6372800 # Radius of earth in meters
+    lat1 = math.radians(self.lat)
+    lon1 = math.radians(self.lon)
+    lat2 = math.radians(p.lat)
+    lon2 = math.radians(p.lon)
+    dLat = lat2-lat1
+    dLon = lon2-lon1
+    a = math.sin(dLat/2) * math.sin(dLat/2) + math.cos(lat1) * math.cos(lat2) * math.sin(dLon/2) * math.sin(dLon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    return R * c;
 
 if __name__ == '__main__':
   import sys
--- test.py
+++ test.py
@@ -7,6 +7,7 @@ from GPX import Route
 from GPX import Waypoint
 import unittest
 import StringIO
+import math
 
 
 class ParseTest(unittest.TestCase):
@@ -44,26 +45,55 @@ class ParseTest(unittest.TestCase):
   
   def testSimpleParse(self):
     self.assertEquals(len(self.gpx.tracks),1)
-    self.assertEquals(len(self.gpx.tracks[0].segments[0].points),3)
-    self.assertEquals(self.gpx.tracks[0].segments[0].points[0].lat,47.644548)
-    self.assertEquals(self.gpx.tracks[0].segments[0].points[0].lon,-122.326897)
+    self.assertEquals(len(self.gpx.waypoints),0)
+    self.assertEquals(len(self.gpx.routes),0)
+    
+  def testTrackIter(self):
+    trk = self.gpx.tracks[0]
+    self.assertEquals(len(trk),1)
+    # points of track is not a list - it is a generator!
+    self.assertEquals(len([x for x in trk.points()]),3)
+
+  def testSegmentIter(self):
+    seg = self.gpx.tracks[0][0]
+    self.assertEquals(len(seg),3)
+    self.assertEquals(seg[0].lat,47.644548)
+    self.assertEquals(seg[0].lon,-122.326897)
+  
+  def testSegmentFilter(self):
+    seg = self.gpx.tracks[0][0]
+    seg.filter(lambda x:True)
+    self.assertEquals(len(seg),3)
+    seg.filter(lambda x:False)
+    self.assertEquals(len(seg),0)
+  
+  def testSegmentFilter(self):
+    seg = self.gpx.tracks[0][0]
+    seg.simplify(lambda a,b:math.fabs(a.ele-b.ele) > 1)
+    self.assertEquals(len(seg),2)
   
   def testTimestamp(self):
-    assert self.gpx.tracks[0].segments[0].timespan() is not None
+    seg = self.gpx.tracks[0][0]
+    assert seg.timespan() is not None
   
   def testBounds(self):
-    assert self.gpx.tracks[0].segments[0].bounds() is not None
+    seg = self.gpx.tracks[0][0]
+    assert seg.bounds() is not None
     
-
+class WaypointTest(unittest.TestCase):
+  def testDist(self):
+    # result taken from http://en.wikipedia.org/wiki/Great-circle_distance
+    d = Waypoint(36.12,-86.67).dist(Waypoint(33.94,-118.40))
+    self.assertAlmostEquals(d,2887260,0)
 
 class WriteTest(unittest.TestCase):
   def testWrite(self):
     g = GPX()
     g.waypoints.append(Waypoint(47.644548,-122.326897))
     p = Path()
-    p.points.append(Waypoint(47.644548,-122.326897))
+    p.append(Waypoint(47.644548,-122.326897))
     t = Track()
-    t.segments.append(p)
+    t.append(p)
     g.tracks.append(t)
     s = StringIO.StringIO()
     g.write(s)
