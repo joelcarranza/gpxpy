a53d4eb2ab9729ae20b3aaac694b839ea551137a
Joel Carranza <joel.carranza@gmail.com>
2011-06-11 16:13:17 -0700

    reorg

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gpxpy',
+      version='0.1',
+      description='GPX library',
+      author='Joel Carranza',
+      author_email='joel.carranza@gmail.com',
+      url='http://carranza-collective.com/joel/',
+      packages=['gpxpy', 'gpxpy.tools'],
+    #  scripts=['scripts/micropress']
+     )
