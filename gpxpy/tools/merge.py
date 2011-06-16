d129cb1d8660d4b0c760501dd8d69aba541a2f1b
Joel Carranza <joel.carranza@gmail.com>
2011-06-15 23:06:24 -0700

    added gpxmerge script

--- gpxpy/tools/merge.py
+++ gpxpy/tools/merge.py
@@ -10,6 +10,8 @@ Created by Joel Carranza on 2011-02-11.
 
 import argparse
 from gpxpy import *
+from datetime import datetime
+import pytz
 
 def merge(files):
   gpx = GPX()
@@ -21,18 +23,20 @@ def merge(files):
   # sort routes by name
   gpx.routes.sort(key=lambda r:r.name or '')  
   
-  # sort tracks by time
-  # TODO: what if they don't have time?
-  gpx.tracks.sort(key=lambda t:t[0][0].time)
+  # sort tracks by time, then name
+  # if there is no time for a particular track - use timezero!
+  timezero = datetime(1,1,1,tzinfo=pytz.utc)
+  gpx.tracks.sort(key=lambda t:(t[0][0].time or timezero,t.name))
   return gpx
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Trim GPX file to time')
-    parser.add_argument('-o', metavar='file',type=argparse.FileType('w'),default=sys.stdout)
-    parser.add_argument('-w', action='store_true')
-    parser.add_argument('-t', action='store_true')
-    parser.add_argument('-r', action='store_true')
-    parser.add_argument('files', metavar='file',nargs="+",type=argparse.FileType('r'),default=sys.stdin)
+
+def run():
+    parser = argparse.ArgumentParser(description='Merge multiple gpx files into one')
+    parser.add_argument('-o', metavar='out-file',type=argparse.FileType('w'),default=sys.stdout)
+    parser.add_argument('-w', action='store_true',help="waypoints only")
+    parser.add_argument('-t', action='store_true',help="tracks only")
+    parser.add_argument('-r', action='store_true',help="routes only")
+    parser.add_argument('files', metavar='file',nargs="+",type=argparse.FileType('r'),default=sys.stdin,help="output file")
     args = parser.parse_args()
     gpx = merge(args.files);
     if args.w or args.t or args.r:
@@ -43,3 +47,7 @@ if __name__ == "__main__":
       if not args.r:
         gpx.route = []
     gpx.write(args.o)
+    
+
+if __name__ == "__main__":
+  run()
