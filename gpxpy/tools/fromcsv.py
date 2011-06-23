2c6189449e21348e8f787c830b5650abfe33751f
Joel Carranza <joel.carranza@gmail.com>
2011-06-22 22:07:30 -0700

    fromcsv fix

--- gpxpy/tools/fromcsv.py
+++ gpxpy/tools/fromcsv.py
@@ -17,26 +17,32 @@ from gpxpy import *
 import pytz
 from csv import DictReader
 
-def main(files,latField,lonField,nameField,typeField):
+def main(file,latField,lonField,nameField,typeField):
   # TODO: optionally turn into track or route
   gpx = GPX()
-  for f in files:
-    reader = DictReader(f)
-    for row in reader:
-        lat = float(row[latField])
-        lon = float(row[lonField])
-        w = gpx.newWaypoint(lat,lon)
-        if nameField is not None:
-          w.name = row[nameField]
-        if typeField is not None:
-          w.type = row[typeField]          
+  reader = DictReader(file)
+  for row in reader:
+      lat = float(row[latField])
+      lon = float(row[lonField])
+      w = gpx.newWaypoint(lat,lon)
+      if nameField is not None:
+        w.name = row[nameField]
+      if typeField is not None:
+        w.type = row[typeField]          
   return gpx
 
-if __name__ == "__main__":
+def run():
   parser = argparse.ArgumentParser(description='Creates a GPX file from a CSV file')
-  parser.add_argument('-i', metavar='file',nargs="+",type=argparse.FileType('rU'),default=sys.stdin)
+  parser.add_argument('-i', metavar='file',type=argparse.FileType('rU'),default=sys.stdin)
   parser.add_argument('-o', metavar='file',type=argparse.FileType('w'),default=sys.stdout)
+  parser.add_argument('-lat',metavar="field",default="LAT")
+  parser.add_argument('-lon',metavar="field",default="LON")
+  parser.add_argument('-name',metavar="field")
+  parser.add_argument('-type',metavar="field")
+  
   args = parser.parse_args()
-  # TODO: these must not be hard coded
-  gpx = main(args.i,'PRIM_LAT_DEC','PRIM_LONG_DEC','FEATURE_NAME','FEATURE_CLASS')
+  gpx = main(args.i,args.lat,args.lon,args.name,args.type)
   gpx.write(args.o)
+  
+if __name__ == "__main__":
+  run()
