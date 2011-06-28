5cd6a996faa04a8e097ab12080f7d8d6f868517c
Joel Carranza <joel.carranza@gmail.com>
2011-06-27 21:47:26 -0700

    added TODOs

--- gpxpy/tools/join.py
+++ gpxpy/tools/join.py
@@ -14,7 +14,9 @@ import argparse
 import gpxpy.tools
 
 def run():
+  # TODO: option to explode individual segments into tracks + rename!
   parser = argparse.ArgumentParser(description='Join GPX tracks together',parents=[gpxpy.tools.inoutargs()])
+  # TODO: explain possible formats here
   parser.add_argument('-t', metavar="duration",type=gpxpy.tools.parse_timedelta,help="Join segments only if points are within this time period")
   parser.add_argument('-d', metavar="dist",type=gpxpy.tools.parse_dist,help="Join segments only if points are within this distance")
   
