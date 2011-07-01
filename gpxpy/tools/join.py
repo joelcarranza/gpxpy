67e385956788f9d2154e9e95ee1c404092438f0f
Joel Carranza <joel.carranza@gmail.com>
2011-06-30 22:18:38 -0700

    added -x options to join

--- gpxpy/tools/join.py
+++ gpxpy/tools/join.py
@@ -14,15 +14,17 @@ import argparse
 import gpxpy.tools
 
 def run():
-  # TODO: option to explode individual segments into tracks + rename!
   parser = argparse.ArgumentParser(description='Join GPX tracks together',parents=[gpxpy.tools.inoutargs()])
-  # TODO: explain possible formats here
   parser.add_argument('-t', metavar="duration",type=gpxpy.tools.parse_timedelta,help="Join segments only if points are within this time period")
   parser.add_argument('-d', metavar="dist",type=gpxpy.tools.parse_dist,help="Join segments only if points are within this distance")
+  # TODO: option to explode individual segments into tracks + rename!
+  parser.add_argument('-x', action='store_true',help="Convert segments into tracks")
   
   args = parser.parse_args()
   gpx = gpxpy.tools.gpxin(args)
   gpx.join(args.d,args.t)
+  if args.x:
+    gpx.segments_to_tracks()
   gpxpy.tools.gpxout(gpx,args)
   
 if __name__ == "__main__":
