a976222c21c730849a9ace94803d3bee1af46736
Joel Carranza <joel.carranza@gmail.com>
2011-07-02 06:44:23 -0700

    documentation and doctests for gpxpy.tools

--- gpxpy/tools/__init__.py
+++ gpxpy/tools/__init__.py
@@ -7,22 +7,34 @@ import re
 
 def inoutargs():
   parser = argparse.ArgumentParser(add_help=False)
-  # TODO: add help to these areguments
+  # TODO: add help to these arguments
   parser.add_argument('-i', metavar='file',type=argparse.FileType('r'),default=sys.stdin)
   parser.add_argument('-o', metavar='file',type=argparse.FileType('w'),default=sys.stdout)
   # TODO: --name and --description tags for any writen out gpx files
   return parser
   
 def gpxin(args):
+  "Given arg setup using inoutargs - load the target GPX file"
   return gpxpy.parse(args.i)
   
 def gpxout(gpx,args):
-  # TODO: ammend with name and description tags
+  "Given arg setup using inoutargs - write the target GPX file"
   gpx.write(args.o)
   
 units_conv = dict(m=1.0,mi=1609.344,ft=0.3048,km=1000)
 
 def parse_dist(str):
+  """
+  Parse a text distance of the form Nunits into meters
+  >>> parse_dist('1m')
+  1.0
+  >>> parse_dist('5mi')
+  8046.7200000000003
+  >>> parse_dist('1500ft')
+  457.20000000000005
+  >>> parse_dist('1.1km')
+  1100.0
+  """
   m = re.match(r"(\d+(?:\.\d+)?)([a-zA-Z]+)?",str)
   if m:
     n = float(m.group(1))
@@ -30,11 +42,30 @@ def parse_dist(str):
     return n*units_conv[units]
   raise Exception("Failed to parse %s" %str)
 
-def parse_timezone(str):
-  # TODO: replace spaces with underscores for more lax parsing
-  return pytz.timezone(str)
+def parse_timezone(tz):
+  """
+  Parse a timezone description into a tzinfo object
+  >>> parse_timezone("America/Los Angeles")
+  <DstTzInfo 'America/Los_Angeles' PST-1 day, 16:00:00 STD>
+  >>> parse_timezone("America/Los_Angeles")
+  <DstTzInfo 'America/Los_Angeles' PST-1 day, 16:00:00 STD>
+  """
+  return pytz.timezone(tz.replace(" ","_"))
 
 def parse_timedelta(str):
+  """
+  Parse time string of the form Nunit into a datetime.timedelta 
+  object
+  
+  >>> parse_timedelta('1h')
+  datetime.timedelta(0, 3600)
+  >>> parse_timedelta('1d')
+  datetime.timedelta(1)
+  >>> parse_timedelta('3d')
+  datetime.timedelta(3)
+  >>> parse_timedelta('60m')
+  datetime.timedelta(0, 3600)
+  """
   m = re.match(r"(\d+)([dhms])",str)
   if m:
     n = int(m.group(1))
